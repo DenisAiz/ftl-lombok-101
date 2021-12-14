ff2312b7ea2f504abeb99d808ab5371c93d687a5
DenAiz <git@github.com:DenisAiz/ftl-jb-001.git>
2021-12-14 15:05:10 +0300

    Task2 - val example

---
+++ maven/src/main/java/Example/Program.java
@@ -0,0 +1,11 @@
+package Example;
+
+public class Program {
+
+    public static void main(String[] args){
+        ValExample valExample = new ValExample();
+
+        System.out.println(valExample.example());
+        System.out.println(valExample.example2());
+    }
+}
