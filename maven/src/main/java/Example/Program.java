7e1e53f8ab8abdb20a60a579b7e5c28d77bae834
DenAiz <git@github.com:DenisAiz/ftl-jb-001.git>
2021-12-14 18:05:51 +0300

    Task6 - getter&setter example

--- maven/src/main/java/Example/Program.java
+++ maven/src/main/java/Example/Program.java
@@ -4,6 +4,7 @@ import Example.Task2.ValExample;
 import Example.Task3.VarExample;
 import Example.Task4.NonNullExample;
 import Example.Task5.CleanUpExample;
+import Example.Task6.GetterSetterExample;
 
 import java.io.IOException;
 
@@ -13,6 +14,9 @@ public class Program {
         ValExample.run();
         VarExample.run();
         NonNullExample.run();
+
+        GetterSetterExample getterSetterExample = new GetterSetterExample();
+        System.out.println(getterSetterExample);
 
         CleanUpExample.run(args);
     }
